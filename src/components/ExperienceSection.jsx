2745d4b0ea55ddf430a9d412ebba17b4905ed903
Estherlvn <estherlvn@gmail.com>
2025-08-06 00:04:50 +0200

    add logo ExperienceSection

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -2,30 +2,34 @@ import './ExperienceSection.css';
 
 const experiences = [
     {
-      badge: 'Titre obtenu',
+      badge: 'Titre RNCP 5 obtenu',
       icon: 'fa-solid fa-graduation-cap',
-      title: 'Développeur web Fullstack',
+      dotImage: '/assets/dots/logo_elan.png',
+      title: 'Formation Développeur web Fullstack',
       company: 'ELAN Formation, Strasbourg',
       date: 'Oct. 2024 - Mai 2025'
     },
     {
       badge: 'Stage conventionné',
       icon: 'fa-solid fa-briefcase',
+      dotImage: '/assets/dots/logo_digicom.png',
       title: 'Stage développement web',
       company: 'Digicom Corporation, Strasbourg',
       date: 'Mars - Mai 2025'
     },
     {
-      badge: 'Titre obtenu',
+      badge: 'Certification obtenue',
       icon: 'fa-solid fa-graduation-cap',
-      title: 'Développeur web Front-end',
+      dotImage: '/assets/dots/logo_simplon.jpg',
+      title: 'Formation Développeur web Front-end',
       company: 'SIMPLON Grand Ouest, Rennes',
       date: 'Juin 2024 - Aout 2024'
     },
     {
       badge: 'Certification obtenue',
       icon: 'fa-solid fa-graduation-cap',
-      title: 'Conception CMS (Wordpress)',
+      dotImage: '/assets/dots/logo_simplon.jpg',
+      title: 'Formation en Conception web (Wordpress)',
       company: 'SIMPLON Grand Ouest, Rennes',
       date: 'Mars 2024 - Avril 2024'
     }
@@ -45,18 +49,21 @@ const ExperienceSection = () => {
         <div className="timeline">
           {experiences.map((exp, index) => (
             <div className="timeline-item" key={index}>
-              <div className="timeline-dot" />
+              <div className="timeline-dot">
+                <img src={exp.dotImage} alt="" />
+              </div>
               <div className="timeline-content">
-  <div className="content-header">
-    <h4>{exp.title}</h4>
-    <div className="badge">
-      <i className={exp.icon}></i>
-      <span>{exp.badge}</span>
-    </div>
+              <div className="content-header">
+  <h4>{exp.title}</h4>
+  <div className="badge">
+    <i className={exp.icon}></i>
+    <span>{exp.badge}</span>
   </div>
-  <p>{exp.company}</p>
-  <span>{exp.date}</span>
 </div>
+
+              <p>{exp.company}</p>
+              <span>{exp.date}</span>
+          </div>
 
             </div>
           ))}
